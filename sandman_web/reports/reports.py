13ea135e94a8d370c6ddb3c1ae8a329d66f5b519
shawn-lindberg <shawn.lindberg@gmail.com>
2026-05-26 22:19:12 -0500

    Report updates. (#11)
    * Significantly re-factored report loading to support only the latest report format.
    
    * Switched from using datetime to whenever for everything.
    
    * Added much better testing for loading reports.
    
    * Made some minor fixes to report page rendering.

--- sandman_web/reports/reports.py
+++ sandman_web/reports/reports.py
@@ -1,18 +1,281 @@
 """Implements the reports list and individual reports webpages."""
 
-import datetime
 import json
+import logging
 import os
+import typing
 from operator import itemgetter
 
 import flask
+import whenever
 from werkzeug.exceptions import abort
+
+_logger = logging.getLogger("sandman.routines")
 
 _report_prefix = "sandman"
 _report_extension = ".rpt"
 
-# The date and time format for report events.
-_report_date_time_format = "%Y/%m/%d %H:%M:%S %Z"
+
+type ReportEventInfo = typing.Mapping[
+    str, typing.Mapping[str, int | str] | int | str
+]
+
+
+class ReportEvent:
+    """An event for a report file."""
+
+    def __init__(self) -> None:
+        """Initialize the report event."""
+        self.__when: whenever.ZonedDateTime | None = None
+        self.__info: ReportEventInfo = {}
+
+    @property
+    def when(self) -> whenever.ZonedDateTime | None:
+        """Get the when."""
+        return self.__when
+
+    @when.setter
+    def when(self, when: whenever.ZonedDateTime) -> None:
+        """Set the when."""
+        if isinstance(when, whenever.ZonedDateTime) == False:
+            raise TypeError("When must be a zoned date/time.")
+
+        self.__when = when
+
+    @property
+    def info(self) -> ReportEventInfo:
+        """Get the info."""
+        return self.__info
+
+    @info.setter
+    def info(self, info: ReportEventInfo) -> None:
+        """Set the info."""
+        if isinstance(info, dict) == False:
+            raise TypeError("Info must be a ReportEventInfo.")
+
+        # This will be more robust if the info is a concrete type.
+        if info == {}:
+            raise ValueError("Info must not be an empty dictionary.")
+
+        self.__info = info
+
+    def is_valid(self) -> bool:
+        """Check whether this is a valid report event."""
+        if self.__when is None:
+            return False
+
+        # This will be a lot more robust if the info becomes a concrete class.
+        if self.__info == {}:
+            return False
+
+        return True
+
+    def __eq__(self, other: object) -> bool:
+        """Check whether this event and another have equal values."""
+        if not isinstance(other, ReportEvent):
+            return NotImplemented
+
+        return (self.__when == other.__when) and (self.__info == other.__info)
+
+    @classmethod
+    def parse_from_string(
+        cls, event_string: str, filename: str
+    ) -> typing.Self:
+        """Parse the event from a (JSON) string."""
+        event = cls()
+
+        try:
+            event_json = json.loads(event_string)
+
+        except json.JSONDecodeError:
+            _logger.warning(
+                "JSON error decoding event for report file '%s'.",
+                filename,
+            )
+            return event
+
+        try:
+            event.when = whenever.ZonedDateTime.parse_iso(event_json["when"])
+
+        except KeyError:
+            _logger.warning(
+                "Missing 'when' key in event in report file '%s'.",
+                filename,
+            )
+            return event
+
+        except (TypeError, ValueError):
+            _logger.warning(
+                "Invalid when '%s' in event in report file '%s'.",
+                str(event_json["when"]),
+                filename,
+            )
+            return event
+
+        try:
+            info = event_json["info"]
+
+        except KeyError:
+            _logger.warning(
+                "Missing 'info' key in event in report file '%s'.",
+                filename,
+            )
+
+        else:
+            if isinstance(info, dict) == True:
+                try:
+                    event.info = dict(info)
+
+                except ValueError:
+                    _logger.warning(
+                        "Invalid info '%s' in event in report file '%s'.",
+                        str(info),
+                        filename,
+                    )
+
+            else:
+                _logger.warning(
+                    "Invalid info '%s' in event in report file '%s'.",
+                    str(info),
+                    filename,
+                )
+
+        return event
+
+
+class Report:
+    """All of the information from a report file."""
+
+    def __init__(self) -> None:
+        """Initialize the report."""
+        self.__version = -1
+        self.__start: whenever.ZonedDateTime | None = None
+        self.__events: list[ReportEvent] = []
+
+    @property
+    def version(self) -> int:
+        """Get the version."""
+        return self.__version
+
+    @version.setter
+    def version(self, version: int) -> None:
+        """Set the version."""
+        if isinstance(version, int) == False:
+            raise TypeError("Version must be an integer.")
+
+        if version < 0:
+            raise ValueError("Cannot set a negative version.")
+
+        self.__version = version
+
+    @property
+    def start(self) -> whenever.ZonedDateTime | None:
+        """Get the start."""
+        return self.__start
+
+    @start.setter
+    def start(self, start: whenever.ZonedDateTime) -> None:
+        """Set the start."""
+        if isinstance(start, whenever.ZonedDateTime) == False:
+            raise TypeError("Start must be a zoned date/time.")
+
+        self.__start = start
+
+    @property
+    def events(self) -> list[ReportEvent]:
+        """Get the events."""
+        return self.__events
+
+    def append_event(self, event: ReportEvent) -> None:
+        """Add an event to the end."""
+        self.__events.append(event)
+
+    def is_valid(self) -> bool:
+        """Check whether this is a valid report."""
+        if self.__version < 0:
+            return False
+
+        if self.__start is None:
+            return False
+
+        return True
+
+    @classmethod
+    def parse_from_file(cls, filename: str) -> typing.Self:
+        """Parse a report from a file."""
+        report = cls()
+
+        try:
+            with open(filename) as file:
+                report_lines = file.readlines()
+
+        except FileNotFoundError as error:
+            _logger.error("Could not find report file '%s'.", filename)
+            raise error
+
+        num_lines = len(report_lines)
+
+        if num_lines == 0:
+            _logger.warning("Report file '%s' is empty.", filename)
+            return report
+
+        # The first line is expected to be the header.
+        try:
+            header_json = json.loads(report_lines[0])
+
+        except json.JSONDecodeError:
+            _logger.error(
+                "JSON error decoding header for report file '%s'.",
+                filename,
+            )
+            return report
+
+        try:
+            report.version = header_json["version"]
+
+        except KeyError:
+            _logger.error("Missing version in report file '%s'.", filename)
+            return report
+
+        except (TypeError, ValueError):
+            _logger.error(
+                "Invalid version '%s' in report file '%s'.",
+                str(header_json["version"]),
+                filename,
+            )
+            return report
+
+        # Don't support loading reports older than version 4.
+        if report.version < 4:
+            return report
+
+        try:
+            report.start = whenever.ZonedDateTime.parse_iso(
+                header_json["start"]
+            )
+
+        except KeyError:
+            _logger.error("Missing start in report file '%s'.", filename)
+            return report
+
+        except (TypeError, ValueError):
+            _logger.error(
+                "Invalid start '%s' in report file '%s'.",
+                str(header_json["start"]),
+                filename,
+            )
+            return report
+
+        # Load the events.
+        for line_index in range(1, num_lines):
+            event = ReportEvent.parse_from_string(
+                report_lines[line_index], filename
+            )
+
+            if event.is_valid() == True:
+                report.append_event(event)
+
+        return report
 
 
 def _get_reports_path() -> str:
@@ -49,19 +312,32 @@ def index() -> str:
 
         date_string = base_name[len(_report_prefix) :]
 
-        # Try to convert the name to a date.
-        date_format = "%Y-%m-%d"
+        # Try to convert the name to a date. Expected format: YYYY-MM-DD
+        date_elements = date_string.split("-")
+
+        if len(date_elements) < 3:
+            continue
 
         try:
-            date = datetime.datetime.strptime(date_string, date_format)
+            year = int(date_elements[0])
+
+        except ValueError:
+            continue
+
+        try:
+            month = int(date_elements[1])
+
+        except ValueError:
+            continue
+
+        try:
+            day = int(date_elements[2])
 
         except ValueError:
             continue
 
         # Add a dictionary containing the date.
-        reports.append(
-            {"year": date.year, "month": date.month, "day": date.day}
-        )
+        reports.append({"year": year, "month": month, "day": day})
 
     # Sort them in descending order.
     sorted_reports = sorted(
@@ -69,101 +345,6 @@ def index() -> str:
     )
 
     return flask.render_template("reports.html", reports=sorted_reports)
-
-
-def _update_report_event_from_version_3(event: dict[any]) -> None:
-    # Handle the schedule -> routine rename on on old data.
-    if event["type"] == "schedule":
-        event["type"] = "routine"
-
-    if event["type"] == "control" and event["source"] == "schedule":
-        event["source"] = "routine"
-
-
-def _make_report_event_from_version_2(event: str) -> dict[any]:
-    # Convert the event into the most likely sort of control event.
-    control_action_parts = event.split(": ")
-    control_name = control_action_parts[0]
-
-    control_action = "stop"
-    if control_action_parts[1] == "moving up":
-        control_action = "move up"
-    elif control_action_parts[1] == "moving down":
-        control_action = "move down"
-
-    converted_event = {
-        "type": "control",
-        "control": control_name,
-        "action": control_action,
-        "source": "command",
-    }
-
-    return converted_event
-
-
-def _parse_report_file(filename: str) -> tuple[int, list[any]]:
-    """Parse a report file.
-
-    Returns a tuple containing the version as the first element (or None if
-    there was an error). The second element of the tuple is a list of tuples.
-    These list elements correspond to events from the report, where the first
-    element is the date and time, and the second is the event information.
-    """
-    version = None
-    infos = []
-
-    try:
-        with open(filename, encoding="utf-8") as report_file:
-            # Process every line of the file.
-            for line_index, line in enumerate(report_file):
-                # Try to convert the line to JSON.
-                try:
-                    line_json = json.loads(line)
-
-                except json.JSONDecodeError:
-                    continue
-
-                # The first line should be the header information.
-                if line_index == 0:
-                    version = line_json.get("version")
-
-                    if version is None:
-                        break
-
-                else:
-                    # Get the date and time and convert it to an object.
-                    line_date_time = line_json.get("dateTime")
-
-                    if line_date_time is None:
-                        continue
-
-                    try:
-                        info_date_time = datetime.datetime.strptime(
-                            line_date_time, _report_date_time_format
-                        )
-
-                    except ValueError:
-                        continue
-
-                    line_event = line_json.get("event")
-
-                    if line_event is None:
-                        line_event = "None"
-
-                    if version == 3:
-                        _update_report_event_from_version_3(line_event)
-
-                    elif version == 2:
-                        line_event = _make_report_event_from_version_2(
-                            line_event
-                        )
-
-                    infos.append((info_date_time, line_event))
-
-    except OSError:
-        return (version, infos)
-
-    return (version, infos)
 
 
 @blueprint.route("/<int:year>/<int:month>/<int:day>/report")
@@ -176,61 +357,66 @@ def report(year: int, month: int, day: int) -> str:
         reports_path + "/" + _report_prefix + report_name + _report_extension
     )
 
-    # Try to generate a report start time to fall back on if we don't get one
-    # from the file.
-    date_format = "%Y-%m-%d"
-
-    try:
-        report_end_date = datetime.datetime.strptime(report_name, date_format)
-
-    except ValueError:
-        abort(404, "Oops!")
-
-    # The start date is one day before.
-    report_start_date_time = report_end_date + datetime.timedelta(
-        days=-1, hours=17
-    )
-
     # Attempt to parse the data in the file.
-    report_version, report_infos = _parse_report_file(report_filename)
+    try:
+        loaded_report = Report.parse_from_file(report_filename)
 
-    if report_version is None:
-        abort(404, "Oops!")
+    except FileNotFoundError:
+        abort(404, "Report not found.")
+
+    if loaded_report.is_valid() == False:
+        abort(404, "Report invalid.")
+
+    report_start = loaded_report.start
 
     # Now that we have pulled data out of the file, do some processing to
     # convert it to what we need for display. Part of that will be converting
     # to dictionaries but also calculating the offset from the start time.
     event_infos = []
 
-    for date_time, event in report_infos:
+    for event in loaded_report.events:
+        if event.is_valid() == False:
+            continue
+
+        event_time = event.when
+
         # Figure out how many seconds from the start this event is.
-        seconds_from_start = int(
-            (date_time - report_start_date_time).total_seconds()
-        )
+        since_start = event_time - report_start
+        seconds_from_start = int(since_start.in_seconds())
 
         event_info = {
-            "year": date_time.year,
-            "month": date_time.month,
-            "day": date_time.day,
-            "hour": date_time.hour,
-            "minute": date_time.minute,
-            "second": date_time.second,
+            "year": event_time.year,
+            "month": event_time.month,
+            "day": event_time.day,
+            "hour": event_time.hour,
+            "minute": event_time.minute,
+            "second": event_time.second,
             "secondsFromStart": seconds_from_start,
-            "event": event,
+            "info": event.info,
         }
 
         event_infos.append(event_info)
 
-    report_start_date_string = report_start_date_time.strftime("%Y-%m-%d")
+    report_end = report_start.add(hours=24)
 
-    # Either generate these from the start time or based on the actual data
-    # set in the future.
-    start_hour = 17
+    report_start_date_string = (
+        f"{report_start.year:04d}-"
+        + f"{report_start.month:02d}-"
+        + f"{report_start.day:02d}"
+    )
+
+    report_end_date_string = (
+        f"{report_end.year:04d}-"
+        + f"{report_end.month:02d}-"
+        + f"{report_end.day:02d}"
+    )
+
+    start_hour = report_start.hour
 
     return flask.render_template(
         "report.html",
         start_date_string=report_start_date_string,
-        end_date_string=report_name,
+        end_date_string=report_end_date_string,
         start_hour=start_hour,
         event_infos=event_infos,
     )
--- tests/reports/test_reports.py
+++ tests/reports/test_reports.py
@@ -3,6 +3,9 @@
 import flask
 import flask.testing
 import pytest
+import whenever
+
+import sandman_web.reports.reports as reports
 
 
 def test_reports(client: flask.testing.FlaskClient) -> None:
@@ -10,13 +13,15 @@ def test_reports(client: flask.testing.FlaskClient) -> None:
     response = client.get("/reports/")
     assert response.status_code == 200
     assert b"/reports/2024/12/8/report" in response.data
+    assert b"/reports/2026/3/2/report" in response.data
 
 
 @pytest.mark.parametrize(
     ("url", "status_code", "message"),
     (
-        ("/reports/2024/12/8/report", 200, b"2024-12-08"),
+        ("/reports/2024/12/8/report", 404, b""),
         ("/reports/2024/12/9/report", 404, b""),
+        ("/reports/2026/3/2/report", 200, b"2026-03-02"),
     ),
 )
 def test_report(
@@ -26,3 +31,358 @@ def test_report(
     response = client.get(url)
     assert response.status_code == status_code
     assert message in response.data
+
+
+_default_report_event_when = None
+_default_report_event_info = {}
+
+
+def _check_default_report_event(event: reports.ReportEvent) -> None:
+    """Check that the report event has default values."""
+    assert event.when == _default_report_event_when
+    assert event.info == _default_report_event_info
+    assert event.is_valid() == False
+
+
+def test_report_event_initialization() -> None:
+    """Test initializing report events."""
+    event = reports.ReportEvent()
+    _check_default_report_event(event)
+
+    with pytest.raises(TypeError):
+        event.when = ""
+    _check_default_report_event(event)
+
+    intended_time = whenever.ZonedDateTime(
+        year=2026,
+        month=3,
+        day=29,
+        hour=18,
+        minute=59,
+        second=59,
+        tz="America/Chicago",
+    )
+    event.when = intended_time
+    assert event.when == intended_time
+    assert event.info == _default_report_event_info
+    assert event.is_valid() == False
+
+    with pytest.raises(TypeError):
+        event.info = 1
+    event.when = intended_time
+    assert event.when == intended_time
+    assert event.info == _default_report_event_info
+    assert event.is_valid() == False
+
+    intended_info: reports.ReportEventInfo = {
+        "type": "control",
+        "control": "test_control",
+        "action": "up",
+        "source": "voice",
+    }
+    event.info = intended_info
+    assert event.when == intended_time
+    assert event.info == intended_info
+    assert event.is_valid() == True
+
+
+_default_report_version = -1
+_default_report_start = None
+
+
+def _check_default_report(report: reports.Report) -> None:
+    """Check that the report has default values."""
+    assert report.version == _default_report_version
+    assert report.start == _default_report_start
+    assert len(report.events) == 0
+    assert report.is_valid() == False
+
+
+def _check_expected_report_events(
+    events: list[reports.ReportEvent],
+    expected_events: list[reports.ReportEvent],
+) -> None:
+    """Check whether report events match expected values."""
+    num_events = len(events)
+    num_expected_events = len(expected_events)
+    assert num_events == num_expected_events
+
+    if num_events != num_expected_events:
+        return
+
+    for index in range(num_events):
+        assert events[index] == expected_events[index]
+
+
+def test_report_initialization() -> None:
+    """Test initializing reports."""
+    report = reports.Report()
+    _check_default_report(report)
+
+    with pytest.raises(TypeError):
+        report.version = ""
+    _check_default_report(report)
+
+    with pytest.raises(ValueError):
+        report.version = -2
+    _check_default_report(report)
+
+    expected_version = 3
+
+    report.version = expected_version
+    assert report.version == expected_version
+    assert report.start == _default_report_start
+    assert len(report.events) == 0
+    assert report.is_valid() == False
+
+    with pytest.raises(TypeError):
+        report.start = ""
+    assert report.version == expected_version
+    assert report.start == _default_report_start
+    assert len(report.events) == 0
+    assert report.is_valid() == False
+
+    start_time = whenever.ZonedDateTime(
+        year=2026,
+        month=3,
+        day=29,
+        hour=17,
+        minute=0,
+        second=0,
+        tz="America/Chicago",
+    )
+    report.start = start_time
+    assert report.version == expected_version
+    assert report.start == start_time
+    assert len(report.events) == 0
+    assert report.is_valid() == True
+
+    # Add some events.
+
+    first_time = whenever.ZonedDateTime(
+        year=2026,
+        month=3,
+        day=29,
+        hour=18,
+        minute=59,
+        second=59,
+        tz="America/Chicago",
+    )
+    first_info: reports.ReportEventInfo = {
+        "type": "control",
+        "control": "test_control",
+        "action": "up",
+        "source": "voice",
+    }
+    first_event = reports.ReportEvent()
+    first_event.when = first_time
+    first_event.info = first_info
+    assert first_event.is_valid() == True
+
+    report.append_event(first_event)
+    assert report.version == expected_version
+    assert report.start == start_time
+    _check_expected_report_events(report.events, [first_event])
+    assert report.is_valid() == True
+
+    second_time = whenever.ZonedDateTime(
+        year=2026,
+        month=3,
+        day=29,
+        hour=19,
+        minute=59,
+        second=59,
+        tz="America/Chicago",
+    )
+    second_info: reports.ReportEventInfo = {
+        "type": "routine",
+        "routine": "wake",
+        "action": "up",
+    }
+    second_event = reports.ReportEvent()
+    second_event.when = second_time
+    second_event.info = second_info
+    assert second_event.is_valid() == True
+
+    report.append_event(second_event)
+    assert report.version == expected_version
+    assert report.start == start_time
+    _check_expected_report_events(report.events, [first_event, second_event])
+    assert report.is_valid() == True
+
+
+def test_report_loading() -> None:
+    """Test loading report files."""
+    path: str = "tests/data/reports/"
+
+    with pytest.raises(FileNotFoundError):
+        report = reports.Report.parse_from_file(path + "a")
+
+    # Empty files cannot be parsed.
+    report = reports.Report.parse_from_file(path + "report_test_empty.rpt")
+    _check_default_report(report)
+
+    # Must have a valid header.
+    report = reports.Report.parse_from_file(
+        path + "report_test_invalid_header.rpt"
+    )
+    _check_default_report(report)
+
+    report = reports.Report.parse_from_file(
+        path + "report_test_missing_version.rpt"
+    )
+    _check_default_report(report)
+
+    report = reports.Report.parse_from_file(
+        path + "report_test_type_version.rpt"
+    )
+    _check_default_report(report)
+
+    report = reports.Report.parse_from_file(
+        path + "report_test_invalid_version.rpt"
+    )
+    _check_default_report(report)
+
+    # Beyond this point we have valid versions.
+    expected_version = 4
+
+    report = reports.Report.parse_from_file(
+        path + "report_test_missing_start.rpt"
+    )
+    assert report.version == expected_version
+    assert report.start == _default_report_start
+    assert len(report.events) == 0
+    assert report.is_valid() == False
+
+    report = reports.Report.parse_from_file(
+        path + "report_test_type_start.rpt"
+    )
+    assert report.version == expected_version
+    assert report.start == _default_report_start
+    assert len(report.events) == 0
+    assert report.is_valid() == False
+
+    report = reports.Report.parse_from_file(
+        path + "report_test_invalid_start.rpt"
+    )
+    assert report.version == expected_version
+    assert report.start == _default_report_start
+    assert len(report.events) == 0
+    assert report.is_valid() == False
+
+    # Now we are testing events.
+    start_time = whenever.ZonedDateTime(
+        year=2026,
+        month=3,
+        day=2,
+        hour=17,
+        minute=0,
+        second=0,
+        tz="America/Chicago",
+    )
+
+    time0 = whenever.ZonedDateTime(
+        year=2026,
+        month=3,
+        day=2,
+        hour=23,
+        minute=44,
+        second=49,
+        tz="America/Chicago",
+    )
+
+    time1 = whenever.ZonedDateTime(
+        year=2026,
+        month=3,
+        day=2,
+        hour=23,
+        minute=44,
+        second=50,
+        tz="America/Chicago",
+    )
+
+    expected_event0 = reports.ReportEvent()
+    expected_event0.when = time0
+    expected_event0.info = {
+        "type": "control",
+        "control": "back",
+        "action": "up",
+        "source": "voice",
+    }
+    assert expected_event0.is_valid() == True
+
+    expected_event1 = reports.ReportEvent()
+    expected_event1.when = time1
+    expected_event1.info = {
+        "type": "control",
+        "control": "back",
+        "action": "down",
+        "source": "voice",
+    }
+    assert expected_event1.is_valid() == True
+
+    expected_events = [expected_event0, expected_event1]
+
+    report = reports.Report.parse_from_file(
+        path + "report_test_event_invalid.rpt"
+    )
+    assert report.version == expected_version
+    assert report.start == start_time
+    _check_expected_report_events(report.events, [expected_event1])
+    assert report.is_valid() == True
+
+    report = reports.Report.parse_from_file(
+        path + "report_test_event_missing_when.rpt"
+    )
+    assert report.version == expected_version
+    assert report.start == start_time
+    _check_expected_report_events(report.events, [expected_event1])
+    assert report.is_valid() == True
+
+    report = reports.Report.parse_from_file(
+        path + "report_test_event_type_when.rpt"
+    )
+    assert report.version == expected_version
+    assert report.start == start_time
+    _check_expected_report_events(report.events, [expected_event1])
+    assert report.is_valid() == True
+
+    report = reports.Report.parse_from_file(
+        path + "report_test_event_invalid_when.rpt"
+    )
+    assert report.version == expected_version
+    assert report.start == start_time
+    _check_expected_report_events(report.events, [expected_event1])
+    assert report.is_valid() == True
+
+    report = reports.Report.parse_from_file(
+        path + "report_test_event_missing_info.rpt"
+    )
+    assert report.version == expected_version
+    assert report.start == start_time
+    _check_expected_report_events(report.events, [expected_event1])
+    assert report.is_valid() == True
+
+    report = reports.Report.parse_from_file(
+        path + "report_test_event_type_info.rpt"
+    )
+    assert report.version == expected_version
+    assert report.start == start_time
+    _check_expected_report_events(report.events, [expected_event1])
+    assert report.is_valid() == True
+
+    report = reports.Report.parse_from_file(
+        path + "report_test_event_invalid_info.rpt"
+    )
+    assert report.version == expected_version
+    assert report.start == start_time
+    _check_expected_report_events(report.events, [expected_event1])
+    assert report.is_valid() == True
+
+    report = reports.Report.parse_from_file(
+        path + "report_test_valid_events.rpt"
+    )
+    assert report.version == expected_version
+    assert report.start == start_time
+    _check_expected_report_events(report.events, expected_events)
+    assert report.is_valid() == True
